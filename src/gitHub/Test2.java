3db4cf087fb0be2f4f7c4a63c4adac0cf30083b3
Ayse3434 <aysegul2013x@gmail.com>
2023-06-08 15:22:14 +0200

    son degisiklik

--- src/gitHub/Test2.java
+++ src/gitHub/Test2.java
@@ -3,6 +3,6 @@ package gitHub;
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Deneme Test2 .");
-        System.out.println("2. deneme");
+        System.out.println("2. deneme son");
     }
 }
