ba34714ceb963dfd09ca5b595aa780bb0e574c47
Ayse3434 <aysegul2013x@gmail.com>
2023-06-14 13:58:02 +0200

    test1 son degisiklik

--- src/gitHub/Test1.java
+++ src/gitHub/Test1.java
@@ -3,5 +3,6 @@ package gitHub;
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Deneme Test1 dir.");
+        System.out.println("Degisiklik yaptim");
     }
 }
